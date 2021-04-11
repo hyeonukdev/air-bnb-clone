8f5dc0a2b795d772eea263b9e3c19c49588e2d51
hyeonukdev <46927191+hyeonukdev@users.noreply.github.com>
2021-04-11 21:00:38 +0900

    User Model - Custom user info and GENDER_CHOISE

--- users/models.py
+++ users/models.py
@@ -1,7 +1,21 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+
 class User(AbstractUser):
 
-    pass
+    """ Custom User Model """
 
+    GENDER_MALE = "male"
+    GENDER_FEMALE = "female"
+    GENDER_OTHER = "other"
+
+    GENDER_CHOICES = (
+        (GENDER_MALE, "Maile"),
+        (GENDER_FEMALE, "Female"),
+        (GENDER_OTHER, "Other")
+    )
+
+    avatar = models.ImageField(null=True)
+    gender = models.CharField(choices=GENDER_CHOICES, max_length=10, null=True)
+    bio = models.TextField(default="")
