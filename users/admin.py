ba7f791ddc35f83646115a00bede36add17c333f
hyeonukdev <46927191+hyeonukdev@users.noreply.github.com>
2021-04-11 21:25:29 +0900

    User Model - customizing UserAdmin Panel use fieldsets

--- users/admin.py
+++ users/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 from . import models
 
 
@@ -6,9 +7,28 @@ from . import models
 # 위와 아래는 동일한 의미
 # admin 패널에 등록하기 위해 decorate를 사용
 # admin.site.register(models.User, CustomUserAdmin)
-class CustomUserAdmin(admin.ModelAdmin):
+class CustomUserAdmin(UserAdmin):
 
-    # admin panel에 목록을 한 번에 보기 위해
-    list_display = ('username', 'gender', 'language', 'currency', 'superhost')
-    # admin panel에서 filter를 추가하기 위해
-    list_filter = ('language', 'currency', 'superhost')
+    # 기존꺼에 추가로 합하려면 이렇게
+    # # admin panel에 목록을 한 번에 보기 위해
+    # list_display = ('username', 'gender', 'language', 'currency', 'superhost')
+    # # admin panel에서 filter를 추가하기 위해
+    # list_filter = ('language', 'currency', 'superhost')
+
+    # 필드를 커스터마이징하려면 이렇게
+    fieldsets = UserAdmin.fieldsets + (
+        (
+            "Custom Profile",
+            {
+                "fields": (
+                    "avatar",
+                    "gender",
+                    "bio",
+                    "birthdate",
+                    "language",
+                    "currency",
+                    "superhost",
+                )
+            }
+        ),
+    )
