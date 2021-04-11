4427136935d76ddfbdf6e923ef53921774e17d0b
hyeonukdev <46927191+hyeonukdev@users.noreply.github.com>
2021-04-11 21:18:27 +0900

    User Model - customizing UserAdmin Panel

--- users/admin.py
+++ users/admin.py
@@ -3,5 +3,12 @@ from . import models
 
 
 @admin.register(models.User)
+# 위와 아래는 동일한 의미
+# admin 패널에 등록하기 위해 decorate를 사용
+# admin.site.register(models.User, CustomUserAdmin)
 class CustomUserAdmin(admin.ModelAdmin):
-    pass
+
+    # admin panel에 목록을 한 번에 보기 위해
+    list_display = ('username', 'gender', 'language', 'currency', 'superhost')
+    # admin panel에서 filter를 추가하기 위해
+    list_filter = ('language', 'currency', 'superhost')
--- users/models.py
+++ users/models.py
@@ -31,7 +31,7 @@ class User(AbstractUser):
         (CURRENCY_KRW, "KRW")
     )
 
-    avatar = models.ImageField(null=True)
+    avatar = models.ImageField(null=True, blank=True)
     gender = models.CharField(choices=GENDER_CHOICES, max_length=10, null=True, blank=True)
     bio = models.TextField(default="")
     birthdate = models.DateTimeField(null=True)
