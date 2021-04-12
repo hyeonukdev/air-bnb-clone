08f837ae13b835f49d6bdfb315903a6d1b88053d
hyeonukdev <lego7407@gmail.com>
2021-04-12 23:04:04 +0900

    add Room - Facilitys, Amenitys, HouseRule

--- rooms/models.py
+++ rooms/models.py
@@ -17,9 +17,27 @@ class AbstractItem(core_models.TimeStampedModel):
 
 
 class RoomType(AbstractItem):
+    ''' RoomType Object Definition '''
 
     pass
 
+
+class Amenity(AbstractItem):
+    ''' Amenity Model Definition '''
+
+    pass
+
+
+class Facility(AbstractItem):
+    ''' Facility Model Definition '''
+
+    pass
+
+
+class HouseRule(AbstractItem):
+    ''' HouseRule Model Definition '''
+
+    pass
 
 class Room(core_models.TimeStampedModel):
     ''' Room Modle Definition '''
@@ -38,7 +56,10 @@ class Room(core_models.TimeStampedModel):
     check_out = models.TimeField()
     instant_book = models.BooleanField(default=False)
     host = models.ForeignKey(user_models.User, on_delete=models.CASCADE)
-    room_type = models.ManyToManyField(RoomType, blank=True)
+    room_type = models.ForeignKey(RoomType, on_delete=models.SET_NULL, null=True)
+    amenities = models.ManyToManyField(Amenity)
+    facilities = models.ManyToManyField(Facility)
+    house_rules = models.ManyToManyField(HouseRule)
 
     def __str__(self):
         return self.name
