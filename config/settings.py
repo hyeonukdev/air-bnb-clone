bc9c28f0573f92a9f5195e1920b0908542c251d2
hyeonukdev <lego7407@gmail.com>
2021-04-13 22:52:29 +0900

    add Reservation - Reservation Model

--- config/settings.py
+++ config/settings.py
@@ -46,7 +46,8 @@ PROJECT_APPS = [
     "users.apps.UsersConfig",
     "rooms.apps.RoomsConfig",
     "core.apps.CoreConfig",
-    "reviews.apps.ReviewsConfig"
+    "reviews.apps.ReviewsConfig",
+    "reservations.apps.ReservationsConfig"
 ]
 
 INSTALLED_APPS = DJANGO_APPS + PROJECT_APPS
