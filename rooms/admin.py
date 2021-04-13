802b4ddad35ada8021465ddb564901becbb7fc0c
hyeonukdev <lego7407@gmail.com>
2021-04-13 23:27:49 +0900

    Admin Panel - Rooms

--- rooms/admin.py
+++ rooms/admin.py
@@ -11,7 +11,23 @@ class ItemAdmin(admin.ModelAdmin):
 @admin.register(models.Room)
 class RoomAdmin(admin.ModelAdmin):
     ''' Room Admin Definition '''
-    pass
+
+    list_display = (
+        "name",
+        "country",
+        "city",
+        "price",
+        "guests",
+        "beds",
+        "bedrooms",
+        "baths",
+        "check_in",
+        "check_out",
+        "instant_book",
+    )
+
+    list_filter = ("instant_book", "city", "country")
+    search_fields = ("=city", "^host__username")
 
 
 @admin.register(models.Photo)
