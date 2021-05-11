e0b5d137a199daf8d5236825d9cb7922432fcc15
hyeonukdev <lego7407@gmail.com>
2021-05-11 22:57:41 +0900

    rooms update ListView

--- rooms/views.py
+++ rooms/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect
+from django.utils import timezone
 from django.core.paginator import Paginator, EmptyPage
+from django.views.generic import ListView
 from . import models
 
 
@@ -29,3 +31,20 @@ def all_rooms(request):
         return render(request, "rooms/home.html", {"page": rooms})
     except EmptyPage:
         return redirect("/")
+
+class HomeView(ListView):
+
+    """ HomeView Definition """
+
+    model = models.Room
+    # template_name = "rooms/room_list.html"
+    paginate_by = 10
+    paginate_orphans = 5
+    ordering = "created"
+    context_object_name = "rooms"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        now = timezone.now()
+        context["now"] = now
+        return context
