868de4ba7d3b895a63a9699f5e3b9bd9a578b527
hyeonukdev <lego7407@gmail.com>
2021-05-24 23:05:30 +0900

    add sign up

--- users/views.py
+++ users/views.py
@@ -40,3 +40,20 @@ class LoginView(FormView):
         if user is not None:
             login(self.request, user)
         return super().form_valid(form)
+
+
+class SignUpView(FormView):
+
+    template_name = "users/signup.html"
+    form_class = forms.SignUpForm
+    success_url = reverse_lazy("core:home")
+    initial = {"first_name": "first_name", "last_name": "last_name", "email": "email"}
+
+    def form_valid(self, form):
+        form.save()
+        email = form.cleaned_data.get("email")
+        password = form.cleaned_data.get("password")
+        user = authenticate(self.request, username=email, password=password)
+        if user is not None:
+            login(self.request, user)
+        return super().form_valid(form)
